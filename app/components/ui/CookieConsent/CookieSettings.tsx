b9cbc8bf622e1028e3faf9f742242956af2bc9ec
Kwasako <danburamayuba@gmail.com>
2024-07-21 22:19:49 +0100

    feat: move cookiesSettings to app/data/ui.ts

--- app/components/ui/CookieConsent/CookieSettings.tsx
+++ app/components/ui/CookieConsent/CookieSettings.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styles from './CookieConsent.module.css';
+import { cookieSettings } from '~/data/ui';
 
 interface CookieSetting {
     title: string;
@@ -72,23 +73,6 @@ const CookieSettings: React.FC = () => {
     const [expandedIndex, setExpandedIndex] = useState<number | null>(null);
     const [checkedStates, setCheckedStates] = useState<{ [key: string]: boolean }>({});
 
-    const cookieSettings = [
-        {
-            title: "Performance Cookies",
-            id: "pc",
-            description: "These cookies allow us to count visits and traffic sources so we can measure and improve the performance of our site. They help us to know which pages are the most and least popular and see how visitors move around the site."
-        },
-        {
-            title: "Functional Cookies",
-            id: "fc",
-            description: "These cookies enable the website to provide enhanced functionality and personalisation. They may be set by us or by third party providers whose services we have added to our pages."
-        },
-        {
-            title: "Targeting Cookies",
-            id: "tc",
-            description: "These cookies may be set through our site by our advertising partners. They may be used by those companies to build a profile of your interests and show you relevant adverts on other sites."
-        }
-    ];
 
     const handleToggle = (index: number) => {
         setExpandedIndex(expandedIndex === index ? null : index);
